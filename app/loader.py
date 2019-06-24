0856211fbb205c9d778fd2a62a200e1d61d3c930
Yash Lakhani <yashlakhani13@gmail.com>
2019-06-24 20:48:06 +0100

    Allow for Windows and non-Windows file paths. Better function names.

--- app/loader.py
+++ app/loader.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from instance.config import DB_URL
-import app.pg_helper as pghelper
+import pg_helper
 import pandas as pd
-import requests, zipfile, io
+import requests, zipfile, io, os
 
 
 def download_metadata(dataset_name):
@@ -12,7 +12,7 @@ def download_metadata(dataset_name):
     z.extractall('data')
 
 
-def load_instruments(filename):
+def parse_instruments(filename):
     instrument_df = pd.read_csv(filename)
     instrument_details = instrument_df["code"].str.split("_", n=1, expand=True)
     instrument_df["exchange"] = instrument_details[0]
@@ -21,12 +21,17 @@ def load_instruments(filename):
     return instrument_df
 
 
-def run(dataset_name, filename):
+def load_instruments(dataset_name, filename):
     download_metadata(dataset_name)
-    instruments_df = load_instruments(filename)
+    instruments_df = parse_instruments(filename)
     engine = create_engine(DB_URL)
-    instruments_df.to_sql('instrument', engine, if_exists='replace', method=pghelper.psql_insert_copy)
+    instruments_df.to_sql('instrument', engine, if_exists='replace', method=pg_helper.psql_insert_copy)
 
 
 if __name__ == "__main__":
-    run('CHRIS', 'data\CHRIS_metadata.csv')
+    if os.name == 'nt':
+        filename = 'data\CHRIS_metadata.csv'
+    else:
+        filename = 'data/CHRIS_metadata.csv'
+
+    load_instruments('CHRIS', filename)
