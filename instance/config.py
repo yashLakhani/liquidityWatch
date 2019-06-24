d3153b5d89c3a892c5f10606e8fa456146cd3844
Yash Lakhani <yashlakhani13@gmail.com>
2019-06-24 23:37:12 +0100

    Updating DB config

--- instance/config.py
+++ instance/config.py
@@ -1,11 +1,12 @@
 API_KEYS = {'QUANDL_API_KEY':'cessVzF1i58n_sWoi-tV'}
 
-POSTGRES_USER = 'postgres'
-POSTGRES_PWD = ''
+POSTGRES_USER = 'lw_user'
+POSTGRES_PWD = '1234'
 POSTGRES_HOST = 'localhost'
-POSTGRES_DB = 'marketdata'
-DB_URL = 'postgresql+psycopg2://{user}:{pw}@{host}/{db}'.format(user=POSTGRES_USER,
+POSTGRES_PORT = '5432'
+POSTGRES_DB = 'referencedb'
+DB_URL = 'postgresql+psycopg2://{user}:{pw}@{host}:{port}/{db}'.format(user=POSTGRES_USER,
                                                                 pw=POSTGRES_PWD,
                                                                 host=POSTGRES_HOST,
+                                                                port=POSTGRES_PORT,
                                                                 db=POSTGRES_DB)
-
