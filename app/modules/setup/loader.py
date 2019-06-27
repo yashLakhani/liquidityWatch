13f18b0af20df333e8a634e591523f186887c56b
Yash Lakhani <yashlakhani13@gmail.com>
2019-06-27 23:52:00 +0100

    Improved Setup Script, Cosmetic Changes (Imports in loader.py)

--- app/modules/setup/loader.py
+++ app/modules/setup/loader.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from instance.config import DB_URL
 from app.modules.setup.pg_helper import psql_insert_copy
-import pandas as pd
-import requests, zipfile, io, os
+import pandas
+import requests
+import zipfile
+import io
+import os
 
 
 def download_metadata(dataset_name):
@@ -13,7 +16,7 @@ def download_metadata(dataset_name):
 
 
 def parse_instruments(filename):
-    instrument_df = pd.read_csv(filename)
+    instrument_df = pandas.read_csv(filename)
     instrument_details = instrument_df["code"].str.split("_", n=1, expand=True)
     instrument_df["exchange"] = instrument_details[0]
     instrument_df["code"] = instrument_details[1]
@@ -26,12 +29,3 @@ def load_instruments(dataset_name, filename):
     instruments_df = parse_instruments(filename)
     engine = create_engine(DB_URL)
     instruments_df.to_sql('instrument', engine, if_exists='replace', method=psql_insert_copy)
-
-
-if __name__ == "__main__":
-    if os.name == 'nt':
-        filename = 'data\CHRIS_metadata.csv'
-    else:
-        filename = 'data/CHRIS_metadata.csv'
-
-    load_instruments('CHRIS', filename)
--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import find_packages, setup
 from app.modules.setup.loader import load_instruments
+import os
+import fnmatch
+
 setup(
     name='flaskr',
     version='1.0.0',
@@ -11,4 +14,22 @@ setup(
     ],
 )
 
-load_instruments('CHRIS', '/Users/yashl/PycharmProjects/liquidityWatch/app/data/CHRIS_metadata.csv')
+
+def find_reference_datafile(pattern, path):
+    result = []
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if fnmatch.fnmatch(name, pattern):
+                result.append(os.path.join(root, name))
+    return result
+
+
+def setup_reference_data(dataset, filename):
+    file_matches = find_reference_datafile(filename, '.')
+    if file_matches:
+        load_instruments(dataset, file_matches[0])
+    else:
+        raise ValueError('No Appropriate File Matching with {}'.format(filename))
+
+
+setup_reference_data(dataset='CHRIS', filename ='CHRIS_metadata.csv')
