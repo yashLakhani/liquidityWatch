04f3e24ea16a9506c0d786ea680cf079c5da0411
Yash Lakhani <yashlakhani13@gmail.com>
2019-06-28 08:02:01 +0100

    Correcting Setup Script

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import find_packages, setup
 from app.modules.setup.loader import load_instruments
-import os
-import fnmatch
+from pathlib import Path, PureWindowsPath
 
 setup(
     name='flaskr',
@@ -14,22 +13,6 @@ setup(
     ],
 )
 
-
-def find_reference_datafile(pattern, path):
-    result = []
-    for root, dirs, files in os.walk(path):
-        for name in files:
-            if fnmatch.fnmatch(name, pattern):
-                result.append(os.path.join(root, name))
-    return result
-
-
-def setup_reference_data(dataset, filename):
-    file_matches = find_reference_datafile(filename, '.')
-    if file_matches:
-        load_instruments(dataset, file_matches[0])
-    else:
-        raise ValueError('No Appropriate File Matching with {}'.format(filename))
-
-
-setup_reference_data(dataset='CHRIS', filename ='CHRIS_metadata.csv')
+filename = PureWindowsPath('./data/CHRIS_metadata.csv')
+correct_path = Path(filename)
+load_instruments('CHRIS', filename)
